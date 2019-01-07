8947291c08497eff1d10e7669f610901e1f6d3f3
lijl2k3 <lijl2k3@163.com>
2019-01-07 17:52:39 +0800

    detail2

--- src/components/News.js
+++ src/components/News.js
@@ -1,30 +1,52 @@
 import React,{Component} from 'react';
 import {Link} from 'react-router-dom';
+import {Table} from 'element-react';
 export default class News extends Component{
     constructor(){
         super();
-        this.state={news:[]};
+        this.state={news:[],
+            columns: [
+                {
+                    label: "Title",
+                    prop: "link",
+
+                },
+                {
+                    label: "Author",
+                    prop: "author",
+                    width: 180
+                },
+                {
+                    label: "Publish Date",
+                    prop: "date",
+                    width:180
+                }
+            ],
+
+        };
     }
 
     componentWillMount(){
         let newsStr=localStorage.getItem('news');
         let news=newsStr?JSON.parse(newsStr):[];
+        news.map(item=>{
+            item.author='lijl';
+            let thedate=new Date();
+            item.date=thedate.getFullYear()+'/'+thedate.getMonth()+1+'/'+thedate.getDate();
+            item.link=<Link to={"/news/detail/"+item.id}>{item.title}</Link>;
+        });
         this.setState({news});
     }
 
     render(){
         return (
 
-            <ul className='list-group'>
-                {this.state.news.map(item=>(
-                    <li className="list-group-item" key={item.id}>
-                        <Link to={"/news/detail/"+item.id}>{item.title}</Link>
-
-                    </li>
-
-                ))
-                }
-            </ul>
+            <Table
+                    style={{width:'100%'}}
+                    columns={this.state.columns}
+                    maxHeight={200}
+                    data={this.state.news}
+             />
 
 
         )
