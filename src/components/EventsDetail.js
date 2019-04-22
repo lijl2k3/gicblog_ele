d83b80643ac35d0529ed311c7e5e484b2dcd2829
lijl2k3 <lijl2k3@163.com>
2019-04-22 18:02:40 +0800

    eventdetails2

--- src/components/EventsDetail.js
+++ src/components/EventsDetail.js
@@ -11,7 +11,7 @@ export default class EventsDetail extends Component{
 
     constructor(){
         super();
-        this.state={info:{},
+        this.state={info:{},attendees:[]
         };
     }
 
@@ -20,12 +20,12 @@ export default class EventsDetail extends Component{
 
         if(res.data.code==200){
             let info=res.data.data;
-            this.setState({info});
+            let attendees=JSON.parse(info.attendees);
+            this.setState({info,attendees});
         }
     }
 
     componentWillMount(){
-        console.log('aaa');
         let id=this.props.match.params.id;
         let data={id:id};
         this.details(data);
@@ -39,6 +39,8 @@ export default class EventsDetail extends Component{
 
         let item=this.state.info;
         let contents=(item.contents);
+        let startDate=new Date(this.state.info.start_date*1000);
+        let endDate=new Date(this.state.info.end_date*1000);
        if(contents!==undefined){
           contents=JSON.parse(contents);
        }
@@ -46,6 +48,7 @@ export default class EventsDetail extends Component{
             <div>
                 <HeadBar/>
                 <BreadcumbBar nav_arr={[{txt: 'Events',to:'/events'},{txt:'Details'}]} back={true} handleBack={this.handleBack} />
+
                 <Layout.Row>
                     <Layout.Col span="12" offset="4">
                         <h2> {item.title} </h2>
@@ -53,27 +56,40 @@ export default class EventsDetail extends Component{
                     <Layout.Col span="12" offset="4">
                        <div dangerouslySetInnerHTML={ {__html:draftjs(contents)} }></div>
                 </Layout.Col>
-                </Layout.Row>
-                {/*{item.pics!=undefined & (item.pics.length>0)&&*/}
-                {/*(<div>aa</div>)*/}
-                {/*}*/}
+                    <Layout.Col span="12" offset="4">
+                        <span><b>Time:</b>{startDate.getFullYear() + '/' + parseInt(startDate.getMonth() + 1) + '/' + startDate.getDate()} - {endDate.getFullYear() + '/' + parseInt(endDate.getMonth() + 1) + '/' + endDate.getDate()}</span>
+                    </Layout.Col>
 
-                {/*{(item.pics!==undefined) &&*/}
-                    {/*(<Layout.Row>*/}
-                        {/*<Layout.Col span="12" offset="4">*/}
-                            {/*<h2>Related Pictures:</h2>*/}
-                            {/*<Layout.Row>*/}
-                            {/*{item.pics.map((pic,key)=>{*/}
-                                    {/*return(*/}
-                                        {/*<Layout.Col span="4" style={{'marginBottom':'30px','marginRight':'20px'}} key={key}>*/}
-                                            {/*<div  style={{width:'120px'}} style={{'border': 'solid #333 1px','padding':'20px'}}><a target='_blank' href={"http://localhost/gicapi/public/static/images/"+item.pic_path+'/'+pic} rel="noopener noreferrer"><img style={{'width':'95%'}} src={"http://localhost/gicapi/public/static/images/"+item.pic_path+'/thumb/'+pic} /></a> </div>*/}
-                                        {/*</Layout.Col>*/}
-                                    {/*)*/}
-                                {/*}*/}
-                            {/*) }*/}
-                            {/*</Layout.Row>*/}
-                        {/*</Layout.Col>*/}
-                    {/*</Layout.Row>) }*/}
+                </Layout.Row>
+
+
+                {(this.state.attendees.length>0) &&
+                    (<Layout.Row>
+                        <Layout.Col span="12" offset="4">
+                            <h2>Attendees:</h2>
+                            <Layout.Row>
+                            {this.state.attendees.map((item,key)=>{
+                                    return(
+                                        <div key={key}>
+                                        <Layout.Col span="24" >
+                                            <div><b>{item.title}</b></div>
+                                            <div><img src={"http://localhost/gicapi/public/static/attendees/"+this.state.info.pic_path+'/thumb/'+item.pic.name} style={{float:"left", width:"120px"}} /><span>{item.intro}</span></div>
+                                        </Layout.Col>
+                                        </div>
+                                    )
+                                }
+                            ) }
+                            </Layout.Row>
+                        </Layout.Col>
+                    </Layout.Row>) }
+
+
+
+                    {/*<div>*/}
+                        {/*{item.attendees!=undefined & (item.attendees.length>0)&&*/}
+                        {/*(<h2>Attendees</h2>)*/}
+                        {/*}*/}
+                    {/*</div>*/}
 
             </div>
 
